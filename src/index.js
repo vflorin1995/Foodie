f99dab44663b566ca40a04bd4f19c2bda79d6c94
vflorin1995 <vflorin1995@gmail.com>
2022-10-18 10:48:24 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import picture from './food.png';
 
 const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=e';
-let section = document.querySelector('.items');
+const section = document.querySelector('.items');
 
 fetch(url)
   .then((response) => response.json())
@@ -10,24 +10,24 @@ fetch(url)
     data = data.meals;
     console.log(data);
     data.forEach((item) => {
-        let container = document.createElement('div');
-        container.classList = 'container';
+      const container = document.createElement('div');
+      container.classList = 'container';
 
-        let image = document.createElement('img');
-        image.classList = 'picture';
-        image.src = picture;
+      const image = document.createElement('img');
+      image.classList = 'picture';
+      image.src = picture;
 
-        let title = document.createElement('div');
-        title.innerText = item.strMeal;
-        title.classList = 'title';
+      const title = document.createElement('div');
+      title.innerText = item.strMeal;
+      title.classList = 'title';
 
-        let commentBtn = document.createElement('button');
-        commentBtn.innerText = 'Comment';
+      const commentBtn = document.createElement('button');
+      commentBtn.innerText = 'Comment';
 
-        let reservationBtn = document.createElement('button');
-        reservationBtn.innerText = 'Reservation';
+      const reservationBtn = document.createElement('button');
+      reservationBtn.innerText = 'Reservation';
 
-        container.append(image, title, commentBtn, reservationBtn);
-        section.append(container);
+      container.append(image, title, commentBtn, reservationBtn);
+      section.append(container);
     });
   });
