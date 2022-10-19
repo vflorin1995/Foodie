a42f930aed186d679aabde228a6980f8c5f8689b
Mudasir Sherwani <mudasir9757@gmail.com>
2022-10-19 12:43:55 +0500

    webpack update and build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
+import popupWindowStyles from './Modules/commPopup.css';
 import picture from './food.png';
+import popupWindow from './Modules/commPopup.js';
+const overlay = document.getElementById('overlay');
 
 const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=e';
 const section = document.querySelector('.items');
@@ -8,7 +11,6 @@ fetch(url)
   .then((response) => response.json())
   .then((data) => {
     data = data.meals;
-    console.log(data);
     data.forEach((item) => {
       const container = document.createElement('div');
       container.classList = 'container';
@@ -23,7 +25,8 @@ fetch(url)
 
       const commentBtn = document.createElement('button');
       commentBtn.innerText = 'Comment';
-      commentBtn.id = 'comment-button';
+      commentBtn.classList = 'commButton';
+      commentBtn.id = item.idMeal;
 
       const reservationBtn = document.createElement('button');
       reservationBtn.innerText = 'Reservation';
@@ -32,3 +35,6 @@ fetch(url)
       section.append(container);
     });
   });
+
+
+
