9cab2f4fdadb23b03f5cc8df1ee7b0426da87116
Mudasir Sherwani <mudasir9757@gmail.com>
2022-10-20 14:26:43 +0500

    Add mock comments js file

---
+++ src/__mocks__/mockComments.js
@@ -0,0 +1,34 @@
+const mockComm = [
+  {
+    item_id: 1,
+    comments: [
+      'Mock Comments 11',
+      'Mock Comments 22',
+      'Mock Comments 33',
+      'Mock Comments 44',
+    ],
+  },
+  {
+    item_id: 2,
+    comments: [
+      'Mock Comments 10',
+      'Mock Comments 20',
+    ],
+  },
+  {
+    item_id: 3,
+    comments: [
+      'Mock Comments 12',
+      'Mock Comments 22',
+      'Mock Comments 32',
+    ],
+  },
+  {
+    item_id: 4,
+    comments: [
+      'Mock Comments 12',
+    ],
+  },
+];
+
+module.exports = mockComm;
